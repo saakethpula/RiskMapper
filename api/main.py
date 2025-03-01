4d0161c3b9bc13919c7fb762d4b659c0d5069c4d
Rahul <rahuldavu23@gmail.com>
2025-03-01 15:34:03 -0500

    gemini call code

--- api/main.py
+++ api/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+import google.generativeai as genai
+import os 
+from dotenv import load_dotenv
+from pydantic import BaseModel
 
+load_dotenv
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+class QueryRequest(BaseModel):
+    prompt: str 
+
+
+def get_ai_response(request: QueryRequest):
+    try: 
+        model = genai.GenerativeModel("gemini-pro")
+        response = model.generate_content(request.prompt)
+        return {"response": response.text}
+    except Exception as e:
+        return {"error": str(e)}
